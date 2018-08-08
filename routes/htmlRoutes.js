b35d174f345adb6da0874111e541bd7b1624fb17
hankzimmer7 <hankzimmer7@gmail.com>
2018-08-08 11:28:36 -0400

    fixes improper merging

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-var path = require("path");
-var db = require("../models");
-
-module.exports = function (app) {
-  // Load index page
-  app.get("/", function (req, res) {
-    res.render("index");
-  });
-
-  // Load profile page and pass in a user by id
-  // Need to modify this to query to database for the info, and use a variable in the url for the username
-  app.get("/profile/:user", function (req, res) {
-    db.User.findOne({
-      where: {
-        userName: req.params.user
-      }
-    }).then(function (userObject) {
-      res.render("profile-info", userObject.dataValues);
-    });
-  });
-
-  app.get("/posts", function (req, res) {
-    res.render("posts");
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
-};
-=======
 // var path = require("path");
 var db = require("../models");
 var express = require("express");
@@ -77,4 +45,3 @@ router.get("*", function(req, res) {
 });
 
 module.exports = router;
->>>>>>> 773bb819b97b56a834272ec05d0fb0573e50ae8e
