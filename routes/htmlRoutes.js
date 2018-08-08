158285669f4d27e323756851e4f4ab2e72d934f1
hankzimmer7 <hankzimmer7@gmail.com>
2018-08-08 11:03:49 -0400

    add posts page, uses handlebars to grab profile data and populate profile page, fixes css link on profile page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,51 +1,30 @@
 var path = require("path");
+var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index");
   });
 
   // Load profile page and pass in a user by id
   // Need to modify this to query to database for the info, and use a variable in the url for the username
-  app.get("/profile/hank", function(req, res) {
-    res.render("profile-info", {
-      name: "Hank",
-      img: "https://hankzimmer7.github.io/assets/images/profile_picture.jpg",
-      license: "none",
-      numberOfJump: 0,
-      homeDropZone: "ATL",
-      bio: "I love the sky, but I can't jump"
+  app.get("/profile/:user", function (req, res) {
+    db.User.findOne({
+      where: {
+        userName: req.params.user
+      }
+    }).then(function (userObject) {
+      res.render("profile-info", userObject.dataValues);
     });
   });
 
-  //Uncomment the below to render a dummy profile not using the database:
-  // app.get("/users/hank", function (req, res) {
-  //   res.render("profile-info", {
-  //     name: "Hank",
-  //     img: "https://hankzimmer7.github.io/assets/images/profile_picture.jpg",
-  //     license: "none",
-  //     numberOfJump: 0,
-  //     homeDropZone: "ATL",
-  //     bio: "I love the sky, but I can't jump"
-  //   });
-  // });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/posts", function (req, res) {
+    res.render("posts");
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
